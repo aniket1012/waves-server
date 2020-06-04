77b874d6c4aad2f66708333d7b3a3d6d7986e90f
Aniket <aniketupatel1@gmail.com>
2020-06-04 16:41:54 -0400

    created get route to sort products by arrival

--- Server/server.js
+++ Server/server.js
@@ -33,6 +33,25 @@ const { admin } = require('./middleware/admin')
 //               PRODUCTS
 //=====================================
 
+// BY ARRIVAL
+ app.get('/app/product/articles', (req,res)=>{
+
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.soryBy ? req.query.soryBy : '_id'
+    let limit = req.query.limit ? req.query.limit : 100
+    
+    Product.find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err)
+        res.send(articles)
+    })
+ })
+
+ 
 
 app.get('/api/product/articles_by_id',(req,res)=>{
     let type = req.query.type 
